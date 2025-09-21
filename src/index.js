3307a16ee5aac1141e8e8383f50c61c6f44f3305
Sctache <27soc12@gmail.com>
2025-09-21 10:53:26 -0300

    Primeiro commit!

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Objetivo: Crie uma classe genérica que represente um herói
+// de uma aventura e que possua as seguintes propriedades:
+//- nome
+//- idade
+//- tipo (ex: guerreiro, mago, monge, ninja )
+
+
+
+//Além disso, deve ter um método chamado atacar que deve atender
+// os seguientes requisitos:
+//- exibir a mensagem: "o {tipo} atacou usando {ataque}"), em que o {tipo}
+// deve ser concatenado com o tipo que está na propriedade da classe.
+
+
+
+//- No {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+//se mago -> no ataque exibir (usou magia)
+//se guerreiro -> no ataque exibir (usou espada)
+//se monge -> no ataque exibir (usou artes marciais)
+//se ninja -> no ataque exibir (usou shuriken)
+
+
+
+//Saída: Ao final deve se exibir uma mensagem:
+//- "o {tipo} atacou usando {ataque}"
